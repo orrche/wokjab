6dc5154a1d5c893271665bc4c765f8553ecd1da1
ghostelf <ghostelf@a4058eb7-c820-0410-9b0b-94ec00e1a786>
2007-08-03 15:55:19 +0000

    The really really start of a history plugin
    git-svn-id: https://wokjab.svn.sourceforge.net/svnroot/wokjab/wokjab/trunk/wokjab@111 a4058eb7-c820-0410-9b0b-94ec00e1a786

---
+++ plugins/History.SQLite/history.cpp
@@ -0,0 +1,44 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+ * wokjab
+ * Copyright (C) Kent Gustavsson 2007 <nedo80@gmail.com>
+ * 
+ * wokjab is free software.
+ * 
+ * You may redistribute it and/or modify it under the terms of the
+ * GNU General Public License, as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ * 
+ * wokjab is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with wokjab.  If not, write to:
+ * 	The Free Software Foundation, Inc.,
+ * 	51 Franklin Street, Fifth Floor
+ * 	Boston, MA  02110-1301, USA.
+ */
+
+#include "history.hpp"
+
+History::History(WLSignal *wls): WoklibPlugin(wls)
+{
+	/*
+	
+	int rc = sqlite3_open((std::string(g_get_home_dir()) + "/.wokjab/history/history.db").c_str(), &db);
+	if( rc ){
+			fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+			sqlite3_close(db);
+	}*/
+}
+
+
+History::~History()
+{
+	
+}
+
+
---
+++ plugins/History.SQLite/history.hpp
@@ -0,0 +1,45 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+ * wokjab
+ * Copyright (C) Kent Gustavsson 2007 <nedo80@gmail.com>
+ * 
+ * wokjab is free software.
+ * 
+ * You may redistribute it and/or modify it under the terms of the
+ * GNU General Public License, as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ * 
+ * wokjab is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with wokjab.  If not, write to:
+ * 	The Free Software Foundation, Inc.,
+ * 	51 Franklin Street, Fifth Floor
+ * 	Boston, MA  02110-1301, USA.
+ */
+
+#ifndef _HISTORY_HPP_
+#define _HISTORY_HPP_
+
+#include <Woklib/WLSignal.h>
+#include <Woklib/WoklibPlugin.h>
+#include <Woklib/WokXMLTag.h>
+
+using namespace Woklib;
+
+class History: public WoklibPlugin 
+{
+public:
+	History(WLSignal *wls);
+	~History();
+protected:
+
+private:
+
+};
+
+#endif // _HISTORY_HPP_
