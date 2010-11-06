68f8e409ed4d0be51e3ee424ac7e35bdc5ebfd7f
Kent Gustavsson <nedo80@gmail.com>
2010-11-07 00:15:03 +0100

    Separated the AdHoc plugin into GUI and nonegui

--- plugins/jep0050-AdHocCommands/AdHoc.cc
+++ plugins/jep0050-AdHocCommands/AdHoc.cc
@@ -35,9 +35,6 @@ AdHoc::AdHoc(WLSignal *wls) : WoklibPlugin(wls)
 	EXP_SIGHOOK("Jabber GUI GetJIDMenu", &AdHoc::Menu, 1000);
 	EXP_SIGHOOK("Jabber AdHoc Start", &AdHoc::Start, 1000);
 	EXP_SIGHOOK("Jabber AdHoc Execute", &AdHoc::Exec, 1000);
-	
-	EXP_SIGHOOK("Jabber XML IQ New command set xmlns:http://jabber.org/protocol/commands", &AdHoc::CommandExec, 1000);
-	EXP_SIGHOOK("Jabber XML Message Normal", &AdHoc::Message, 200);
 }
 
 
@@ -45,53 +42,6 @@ AdHoc::~AdHoc()
 {
 
 
-}
-
-int
-AdHoc::Message(WokXMLTag *tag)
-{
-	std::list<WokXMLTag *>::iterator msg_iter;
-	
-	for( msg_iter = tag->GetTagList("message").begin() ; msg_iter != tag->GetTagList("message").end() ; msg_iter++)
-	{
-		std::list<WokXMLTag *>::iterator query_iter;
-		for( query_iter = (*msg_iter)->GetTagList("query").begin() ; query_iter != (*msg_iter)->GetTagList("query").end() ; query_iter++)
-		{
-			if ( (*query_iter)->GetAttr("xmlns") == "http://jabber.org/protocol/disco#items" &&
-							 (*query_iter)->GetAttr("node") == "http://jabber.org/protocol/commands")
-			{
-				WokXMLTag &coms_tag = tag->AddTag("command");
-				coms_tag.AddAttr("name", "Ad-Hoc");
-				
-				std::list<WokXMLTag *>::iterator item_iter;
-				for( item_iter = (*query_iter)->GetTagList("item").begin() ; item_iter != (*query_iter)->GetTagList("item").end() ; item_iter++)
-				{
-					if ( (*msg_iter)->GetAttr("from") != (*item_iter)->GetAttr("jid") )
-					{
-						woklib_message(wls, (*msg_iter)->GetAttr("from") + " is sending you adhoc messages with a different return address");
-						continue;
-					}
-					
-					WokXMLTag &com_tag = coms_tag.AddTag("command");
-					com_tag.AddAttr("name", (*item_iter)->GetAttr("name"));
-					
-					WokXMLTag &sig_tag = com_tag.AddTag("signal");
-					sig_tag.AddAttr("name", "Jabber AdHoc Execute");
-					WokXMLTag &msg_tag = sig_tag.AddTag("message");
-					msg_tag.AddAttr("session", tag->GetAttr("session"));
-					WokXMLTag &iq_tag = msg_tag.AddTag("iq");
-					iq_tag.AddAttr("type", "set");
-					iq_tag.AddAttr("to", (*item_iter)->GetAttr("jid"));
-					WokXMLTag &command_tag = iq_tag.AddTag("command");
-					command_tag.AddAttr("xmlns", "http://jabber.org/protocol/commands");
-					command_tag.AddAttr("node", (*item_iter)->GetAttr("node"));
-					command_tag.AddAttr("action", "execute");
-				}
-			}
-		}
-	}
-
-	return 1;
 }
 
 int
@@ -138,13 +88,6 @@ AdHoc::Menu(WokXMLTag *tag)
 	item.AddAttr("signal", "Jabber AdHoc Start");
 
 
-	return 1;
-}
-
-int
-AdHoc::CommandExec(WokXMLTag *tag)
-{
-	wls->SendSignal("Jabber AdHoc Command Exec " + tag->GetFirstTag("iq").GetFirstTag("command").GetAttr("node"), tag);
 	return 1;
 }
 
